1c88f7d8f527657b3b7b1523b98082ef94e2c2dc
frescodevops <78380605+frescodevops@users.noreply.github.com>
2021-02-17 07:07:04 -0500

    Update ftat.java
    changes for QA environment - correction

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -17,7 +17,7 @@ public class ftat {
 	//public WebDriver driver;
 	public WebDriverWait wait;
 	public String URL = "http://18.218.40.140:8080/QAWebapp/";
-	public String URL1 = "http://18.218.40.140:808018.218.40.140avncreatepage.jsp";
+	public String URL1 = "http://18.218.40.140:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
