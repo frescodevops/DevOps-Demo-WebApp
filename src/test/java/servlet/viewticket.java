7de2e83c0bec0d855f5b2890b4e10d9729368fef
frescodevops <78380605+frescodevops@users.noreply.github.com>
2021-02-18 15:23:02 -0500

    Update viewticket.java
    changed ip address

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.218.40.140:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://3.18.215.122:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
