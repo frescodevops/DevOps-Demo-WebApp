5499236879e382a0e27f2907cff6b537f9f419cb
frescodevops <78380605+frescodevops@users.noreply.github.com>
2021-02-19 08:17:24 -0500

    Update cancelpage.java
    ip changes

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://3.18.215.122:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://3.21.52.238:5432/postgres";
 					String username="postgres";
 					String password="password";
 					
